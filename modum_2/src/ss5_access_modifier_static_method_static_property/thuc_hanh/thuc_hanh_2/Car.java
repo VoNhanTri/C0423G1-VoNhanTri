d4bc08af8db5b32357993a281dbf9f4571508573
VoNhanTri <nhantri249@gmail.com>
2023-05-11 21:15:18 +0700

    nop bai

---
+++ modum_2/src/ss5_access_modifier_static_method_static_property/thuc_hanh/thuc_hanh_2/Car.java
@@ -0,0 +1,24 @@
+package ss5_access_modifier_static_method_static_property.thuc_hanh.thuc_hanh_2;
+
+public class Car {
+    private String name;
+
+    private String engine;
+
+
+
+    public static int numberOfCars;
+
+
+
+    public Car(String name, String engine) {
+
+        this.name = name;
+
+        this.engine = engine;
+
+        numberOfCars++;
+
+    }
+
+}
---
+++ modum_2/src/ss5_access_modifier_static_method_static_property/thuc_hanh/thuc_hanh_2/TestStaticProperty.java
@@ -0,0 +1,14 @@
+package ss5_access_modifier_static_method_static_property.thuc_hanh.thuc_hanh_2;
+
+public class TestStaticProperty {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+
+        System.out.println(Car.numberOfCars);
+
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+
+        System.out.println(Car.numberOfCars);
+
+    }
+}

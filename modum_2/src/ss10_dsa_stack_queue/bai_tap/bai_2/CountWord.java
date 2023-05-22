beec809d22d224e71cc5382b51c3c55d8876e039
VoNhanTri <nhantri249@gmail.com>
2023-05-22 20:14:43 +0700

    nop bai

--- modum_2/src/ss10_dsa_stack_queue/bai_tap/bai_2/CountWord.java
+++ modum_2/src/ss10_dsa_stack_queue/bai_tap/bai_2/CountWord.java
@@ -1,5 +1,25 @@
 package ss10_dsa_stack_queue.bai_tap.bai_2;
 
-public class CountWord {
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
+public class CountWord {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new TreeMap<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập chuỗi để kiểm tra ");
+        String string = scanner.nextLine();
+        String[] array = string.split(" ");
+        for (String s : array) {
+            String str = s.toLowerCase(Locale.ROOT);
+            if (map.containsKey(str)) {
+                map.put(str, map.get(str) + 1);
+            }else {
+                map.put(str, 1);
+            }
+        }
+        System.out.println(map);
+    }
 }

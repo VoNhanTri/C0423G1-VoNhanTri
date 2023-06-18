988c29ea63ec53e8f186c8a236709bc026f35b9b
Nhân Trí Võ <nhantri249@gmail.com>
2023-06-18 16:26:25 +0700

    nop bai

--- module_3/case_study_module_3/truy_van.sql
+++ module_3/case_study_module_3/truy_van.sql
@@ -45,11 +45,47 @@ left join contract_detailed cd on con.id_contract = cd.id_contract
 left join accompanied_service aser on cd.id_accompanied_service = aser.id_accompanied_service;
 
 
+ -- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
+ -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+
+select s.id_service, s.name_service, s.area_service,s.expense_service ,ts.name_type_service
+from service s
+left join type_service ts on s.id_type_service = ts.id_type_service;
 
 
+-- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu 
+-- của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+
+select s.id_service, s.name_service, s.area_service,s.maximum_service,s.expense_service ,ts.name_type_service
+from service s
+left join type_service ts on s.id_type_service = ts.id_type_service;
 
 
+-- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+
+select distinct name_customer
+from customer;
+
+select name_customer
+from customer 
+group by name_customer;
 
 
+-- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+select year(date_contract) as year, month(date_contract) as month, count(*) as count
+from contract
+where  (date_contract like "2021%")
+group by year(date_contract), month(date_contract) 
+order by month;
 
+
+-- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong,
+--  ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+ 
+ select con.id_contract, con.date_contract, con.date_end, con.deposits, sum(cd.quantity)as sum_detailed
+from contract con
+left join contract_detailed cd on con.id_contract = cd.id_contract 
+left join accompanied_service ac on cd.id_accompanied_service = ac.id_accompanied_service
+ group by con.id_contract, con.date_contract, con.date_end, con.deposits;
+ 
 

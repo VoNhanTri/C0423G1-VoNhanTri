510de69b7b751f525a1f8569caedf11e19c62e8e
Nhân Trí Võ <nhantri249@gmail.com>
2023-10-17 11:52:08 +0700

    nop bai

---
+++ module_5.1/bai_1/ex_2.js
@@ -0,0 +1,29 @@
+let courses = [
+    {
+        id: 1,
+        title: "ReactJS Tutorial",
+        rating: 4.2,
+    },
+    {
+        id: 2,
+        title: "Angular Tutorial",
+        rating: 2.5,
+    },
+    {
+        id: 3,
+        title: "VueJS Tutorial",
+        rating: 3.8,
+    },
+    {
+        id: 4,
+        title: "Java Tutorial",
+        rating: 4,
+    },
+    {
+        id: 5,
+        title: "JavaScript Tutorial",
+        rating: 3.5,
+    },
+];
+let arr = courses.filter(temp => temp.rating<4).map(te=>te.id + "-" + te.title + "-" + te.rating);
+console.log(arr)
